09dce02384facbe9350065f3e3b33d6250f570af
sklalaludek <pattiegolis@gmail.com>
2018-02-06 18:59:58 +0100

    ex 1-2

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,19 @@
 //Twój kod
+const fs = require('fs');
+
+fs.readFile('./data/zadanie01/input.json', 'utf8', (error, data) => {
+    if (error === null) {
+        const txt = JSON.parse(data).reduce((a,b) => {
+            return a + b;
+        });
+        fs.writeFile('./data/zadanie01/sum.txt', txt, error => {
+            if (error === null) {
+                console.log('Poprawnie zapisano i odczytano plik.', txt);
+            } else {
+                console.log('Błąd podczas odczytu pliku!', error);
+            }
+        });
+    } else {
+        console.log('Błąd podczas odczytu pliku!', error);
+    }
+});
--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,20 @@
 //Twój kod
+const fs = require('fs');
+
+fs.readdir('./data/zadanie02/', (error, files) => {
+    if (error === null) {
+        console.log('Lista plików:');
+        files.forEach(file => {
+            console.log(file);
+            fs.readFile(`./data/zadanie02/${file}`, 'utf8', (error, data) => {
+                if (error === null) {
+                    console.log('Zawartość pliku: ', data);
+                } else {
+                    console.log('Błąd podczas odczytu pliku!', error);
+                }
+            });
+        });
+    } else {
+        console.log('Błąd podczas listowania katalogu!', error);
+    }
+});
