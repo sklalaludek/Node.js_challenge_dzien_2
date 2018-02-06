be9f110d3eee80cd0078d5239d543ccc149f63dd
sklalaludek <pattiegolis@gmail.com>
2018-02-06 20:00:10 +0100

    zadanie dnia 2

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,20 @@
 //Twój kod
+const fs = require('fs');
+
+let fileName = process.argv.slice(2);
+
+fs.readFile(`${fileName[0]}`, 'utf8', (error, data) => {
+    if (error === null) {
+        let newData = data.split('').map((char, i) => i % 2 === 0 ? char.toUpperCase() : char.toLowerCase());
+        newData = newData.join('');
+        fs.writeFile(`${fileName[0]}`, newData, error => {
+            if (error === null) {
+                console.log('Poprawnie zapisano i odczytano plik.', newData);
+            } else {
+                console.log('Błąd podczas zapisu pliku!', error);
+            }
+        });
+    } else {
+        console.log('Błąd podczas odczytu pliku!', error);
+    }
+});
